375b611bed2cce5642af101e1108aa591bd5ebca
David Shi <dshi@utexas.edu>
2017-04-10 18:00:50 -0500

    changed int to 2float

--- descriptor.py
+++ descriptor.py
@@ -1,5 +1,4 @@
 from random import randint
-from math import floor
 
 # DISCHARGE  = ['bottles','lawns','spit','taps','bathtubs','toilets','milk','punch']
 DISCHARGE = {'bottles': lambda x: x*56.6337}
@@ -8,7 +7,7 @@ GAGEHEIGHT = {'cars'   : lambda x: x*4/12.0, \
               'buses'  : lambda x: x*10/12.0, \
               'Shaquille O\'Neals': lambda x: x/85.0}
 
-ADJECTIVE = [' approximately', ' roughly'      , ' more or less', ' almost'      , ' practically']
+ADJECTIVE = [' approximately', ' roughly'      , ' more or less', ' almost'      , ' practically','']
 EQUALITY  = [' the same as'  , ' equivalent to', ' identical to', ' analogous to', ' parallel to']
     
 def make_descriptions():
@@ -19,20 +18,23 @@ def make_descriptions():
     gage_text(gage_height)
 
 def discharge_text(discharge_rate):
-    description = "The discharge rate is currently " + str(discharge_rate) + " cubic feet per second."
-    description += convert(DISCHARGE, discharge_rate)
+    description = current("discharge rate", discharge_rate, "cubic feet per second")
+    description += convert(DISCHARGE, discharge_rate)[:-1] + " per second!"
     print(description)
 
-
 def gage_text(gage_height):
-    description = "The gage height is currently " + str(gage_height) + " inches."
+    description = current("gage height", gage_height, "inches")
     description += convert(GAGEHEIGHT, gage_height)
     print(description)
 
+def current(strVal, val, strValUnits):
+    return "The " + strVal + " is currently " + str(val) + " " + strValUnits + "."
+
 def convert(graph, val):
     word = list(graph)[randint(0,len(graph) - 1)]
-    conversion = int(round(graph[word](val), 0))
-    return " That's" + ADJECTIVE[randint(0,4)] + EQUALITY[randint(0,4)] + " " + str(conversion) + " " + word + "!"
+    conversion = round(graph[word](val), 2)
+    conversion = int(conversion) if int(conversion) == conversion else conversion
+    return " That's" + ADJECTIVE[randint(0,5)] + EQUALITY[randint(0,4)] + " " + str(conversion) + " " + word + "!"
 
 if __name__ == "__main__":
     make_descriptions()
