af350a9165ba5956e9e9d92c15f0da8b56f6666d
David Shi <dshi@utexas.edu>
2017-04-14 17:20:14 -0500

    added reqs. desc just needs 3 conversion lambdas

--- descriptor.py
+++ descriptor.py
@@ -1,30 +1,37 @@
-from random import randint
+from random import uniform,randint
 
-# DISCHARGE  = ['bottles','lawns','spit','taps','bathtubs','toilets','milk','punch']
-DISCHARGE = {'bottles': lambda x: x*56.6337}
-# GAGEHEIGHT = ['cars','bus','shaqs','whale','giraffe','trex','house']
-GAGEHEIGHT = {'cars'   : lambda x: x*4/12.0, \
-              'buses'  : lambda x: x*10/12.0, \
-              'Shaquille O\'Neals': lambda x: x/85.0}
+# "lawns"   : (lambda x: x*145.29*6048000, "That's enough to water @ lawns per week!"),
+DISCHARGE = {"bottles" : (lambda x: x*56.6337,"That's like pouring @ bottles per second!"), 
+             "spit"    : (lambda x: x, "That's the same as @ people spitting at the same time!"),
+             "taps"    : (lambda x: x*448.8312, "That's roughly equivalent to @ sinks turned on at the same time!"),
+             "bathtubs": (lambda x: x*314.18184, "That's enough water to fill @ bathtubs per second!"),
+             "toilets" : (lambda x: x/0.0267361, "That's the same rate as @ toilets being flushed at the same time!"),
+             "milk"    : (lambda x: x, "If Barton Springs was milk, you would need to eat @ boxes of cereal per second!"),
+             "punch"   : (lambda x: x, "That's the same force as getting punched by @ average men!")}
 
-ADJECTIVE = [' approximately', ' roughly'      , ' more or less', ' almost'      , ' practically','']
-EQUALITY  = [' the same as'  , ' equivalent to', ' identical to', ' analogous to', ' parallel to']
-    
+GAGEHEIGHT = {"cars"    : (lambda x: x/5.0, "That's approximately @ cars stacked on top each other!"),  
+              "buses"   : (lambda x: x/10.0, "That's approximately @ buses stacked on top each other!"),
+              "Shaquille O'Neals": (lambda x: x/85.0, "That's @ Shaquille O'Neals stacked from head to toe!"),
+              "whale"   : (lambda x: x > 20, "That's deep enough for a blue whale to swim in!"),
+              "giraffe" : (lambda x: x > 16, "That's deep enough for a giraffe to swim in!"),
+              "trex"    : (lambda x: x > 20, "That means a T-Rex could take a bath in Barton Springs!"),
+              "house"   : (lambda x: x > 20, "That's enough to submerge a 2-story house!")}
+
 def make_descriptions():
-    discharge_rate = randint(95, 105)
-    gage_height    = randint(23, 25)
+    discharge_rate = round(uniform(9, 140),2)
+    gage_height    = round(uniform(18, 30),2)
 
     discharge_text(discharge_rate)
     gage_text(gage_height)
 
 def discharge_text(discharge_rate):
     description = current("discharge rate", discharge_rate, "cubic feet per second")
-    description += convert(DISCHARGE, discharge_rate)[:-1] + " per second!"
+    description += " " + convert(DISCHARGE, discharge_rate)
     print(description)
 
 def gage_text(gage_height):
-    description = current("gage height", gage_height, "inches")
-    description += convert(GAGEHEIGHT, gage_height)
+    description = current("gage height", gage_height, "feet")
+    description += " " + convert(GAGEHEIGHT, gage_height)
     print(description)
 
 def current(strVal, val, strValUnits):
@@ -32,9 +39,13 @@ def current(strVal, val, strValUnits):
 
 def convert(graph, val):
     word = list(graph)[randint(0,len(graph) - 1)]
-    conversion = round(graph[word](val), 2)
+    
+    while not graph[word][0](val) and graph == GAGEHEIGHT:
+        word = list(graph)[randint(0,len(graph) - 1)]
+
+    conversion = round(graph[word][0](val), 2)
     conversion = int(conversion) if int(conversion) == conversion else conversion
-    return " That's" + ADJECTIVE[randint(0,5)] + EQUALITY[randint(0,4)] + " " + str(conversion) + " " + word + "!"
+    return graph[word][1].replace("@",str(conversion))
 
 if __name__ == "__main__":
     make_descriptions()
